fac75a098b4e6c391e471c207ca8731017c2d1ac
ruijadom <ruijadomingues@gmail.com>
2020-03-03 18:23:02 +0000

    add port

--- config/paths.js
+++ config/paths.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
+  // Webpack
   entryPath: path.resolve(__dirname, '../', 'src/index.js'),
   outputPath: path.resolve(__dirname, '../', 'dist'),
   templatePath: path.resolve(__dirname, '../', 'src/index.html'),
+  // HOST
   publicDevPath: '0.0.0.0',
-  publicProdPath: 'https://ruijadom.github.io/front-end-code-challenge'
+  publicProdPath: 'https://ruijadom.github.io/front-end-code-challenge',
+  // PORT
+  port: '8081'
 };
