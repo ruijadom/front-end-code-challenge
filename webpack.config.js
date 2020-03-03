d6f00047653e0622d8f4676477406332afd24d37
ruijadom <ruijadomingues@gmail.com>
2020-03-03 18:43:05 +0000

    webpack module

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,147 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const commonPaths = require('./config/paths');
 
 module.exports = function(_env, argv) {
-  // define HOST
   const HOST = argv.HOST;
+  const PORT = argv.PORT;
 
   const isProduction = argv.mode === 'production';
   const isDevelopement = !isProduction;
 
   argv.HOST = isDevelopement ? commonPaths.publicDevPath : commonPaths.publicProdPath;
   argv.PORT = commonPaths.PORT;
+
+  return {
+    devtool: isDevelopment && 'cheap-module-source-map',
+    entry: commonPaths.entryPath,
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'scripts/[name].[contenthash:8].js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? 'production' : 'development'
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            'postcss-loader'
+          ],
+          include: [/fonts/]
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'static/media/[name].[hash:8].[ext]'
+            }
+          }
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack']
+        },
+        {
+          test: /\.(eot|otf|ttf|woff|woff2)$/,
+          loader: require.resolve('file-loader'),
+          include: [/fonts/],
+          options: {
+            name: 'static/media/[name].[hash:8].[ext]'
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      isProduction &&
+        new MiniCssExtractPlugin({
+          filename: 'assets/css/[name].[contenthash:8].css',
+          chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css'
+        }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        favicon: path.resolve(__dirname, 'public/favicon.ico'),
+        inject: true
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+      })
+    ].filter(Boolean),
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: {
+              comparisons: false
+            },
+            mangle: {
+              safari10: true
+            },
+            output: {
+              comments: false,
+              ascii_only: true
+            },
+            warnings: false
+          }
+        }),
+        new OptimizeCssAssetsPlugin()
+      ],
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        maxInitialRequests: 10,
+        maxAsyncRequests: 10,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `${cacheGroupKey}.${packageName.replace('@', '')}`;
+            }
+          },
+          common: {
+            minChunks: 2,
+            priority: -10
+          }
+        }
+      },
+      runtimeChunk: 'single'
+    },
+    devServer: {
+      host: HOST,
+      port: PORT,
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      overlay: true
+    }
+  };
 };
