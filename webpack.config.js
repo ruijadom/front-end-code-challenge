2530d9778ad1839d958a49f4ca2f69c84d412b92
ruijadom <ruijadomingues@gmail.com>
2020-03-03 18:21:14 +0000

    define production && developement argv

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,14 @@ const webpack = require('webpack');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const commonPaths = require('./config/paths');
+
+module.exports = function(_env, argv) {
+  // define HOST
+  const HOST = argv.HOST;
+
+  const isProduction = argv.mode === 'production';
+  const isDevelopement = !isProduction;
+
+  argv.HOST = isDevelopement ? commonPaths.publicDevPath : commonPaths.publicProdPath;
+  argv.PORT = commonPaths.PORT;
+};
