c84cad2586cfdf3cb93258dfda046a00e9586a73
ruijadom <ruijadomingues@gmail.com>
2020-03-04 22:23:57 +0000

    svgs

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import LegendList from './legend-list/index';
+import LegendList from './LegendList/LegendList';
 
 import './style.scss';
 
