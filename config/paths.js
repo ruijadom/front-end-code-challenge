a70996222a69cd2bf4edc436f79c371500c936c7
ruijadom <ruijadomingues@gmail.com>
2020-03-04 15:12:02 +0000

    faviconPath and templatePath

--- config/paths.js
+++ config/paths.js
@@ -1,12 +1,11 @@
 const path = require('path');
-const packageJSON = require('../package.json');
 
 module.exports = {
   // Webpack
   entryPath: path.resolve(__dirname, '../', 'src/index.js'),
   outputPath: path.resolve(__dirname, '../', 'dist'),
-  templatePath: path.resolve(__dirname, '../', 'src/index.html'),
-  // publicPath
-  publicDevPath: '/',
-  publicProdPath: packageJSON.homepage
+  templatePath: path.resolve(__dirname, '../', 'public/index.html'),
+  faviconPath: path.resolve(__dirname, '../', 'public/favicon.ico'),
+  outputScripts: 'scripts',
+  outputStatic: 'static/media/'
 };
