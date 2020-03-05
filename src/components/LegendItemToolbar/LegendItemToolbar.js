098fdc2adb6ab5f6f1b0207ffeb4ffbfaa5359c9
ruijadom <ruijadomingues@gmail.com>
2020-03-05 21:50:04 +0000

    stable

--- src/components/LegendItemToolbar/LegendItemToolbar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './LegendItemToolbar.scss';
-
-function LegendItemToolbar() {
-  return <div className="legend-item-toolbar ">Toolbar</div>;
-}
-
-export default LegendItemToolbar;
