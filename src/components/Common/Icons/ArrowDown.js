01aa5f860b814386ade01e120fe5bd8a210cc92d
ruijadom <ruijadomingues@gmail.com>
2020-03-05 21:49:03 +0000

    icon as components

---
+++ src/components/Common/Icons/ArrowDown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ArrowDown({ size, fill }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 32 32" fill={fill}>
+      <path d="M3.75 4.875l12.25 12.25 12.25-12.25 3.75 3.75-16 16-16-16z"></path>
+    </svg>
+  );
+}
+
+ArrowDown.propTypes = {
+  size: PropTypes.number.isRequired,
+  fill: PropTypes.string.isRequired
+};
+
+export default ArrowDown;
