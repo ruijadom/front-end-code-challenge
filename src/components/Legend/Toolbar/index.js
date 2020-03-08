f8134c529a6104a147975f6c29d9cda2c607e503
ruijadom <ruijadomingues@gmail.com>
2020-03-08 18:43:30 +0000

    tooltip

--- src/components/Legend/Toolbar/index.js
+++ src/components/Legend/Toolbar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import Tooltip from 'rc-tooltip';
 import ArrowDown from '../../Common/Icons/arrow-down';
 import ShowIcon from '../../Common/Icons/show';
 import HideIcon from '../../Common/Icons/hide';
 import InfoIcon from '../../Common/Icons/info';
-
+import '../../Common/Tooltip/';
 import './style.scss';
 
 const eye = '#393F44';
@@ -15,29 +16,43 @@ const down = '#333333';
 function Toolbar({ onChangeVisibility, onChangeInfo, onChangeCollapse, isCollapse, isVisible }) {
   return (
     <div className="toolbar">
-      {isVisible ? (
-        <ShowIcon
-          className={classnames('toolbar-icon', isVisible && 'showTooltip')}
-          size={14}
-          fill={eye}
-          onClick={onChangeVisibility}
-        />
-      ) : (
-        <HideIcon
-          className={classnames('toolbar-icon', isVisible && 'showTooltip')}
-          size={14}
-          fill={eye}
-          onClick={onChangeVisibility}
-        />
-      )}
-      <InfoIcon className="toolbar-icon" size={14} fill={info} onClick={onChangeInfo} />
+      <Tooltip
+        overlayClassName="c-rc-tooltip -default"
+        placement="top"
+        trigger={['hover']}
+        overlay={<span>{isVisible ? 'Show layer' : 'Hide layer'}</span>}
+        destroyTooltipOnHide
+      >
+        {isVisible ? (
+          <ShowIcon className="toolbar-icon" size={14} fill={eye} onClick={onChangeVisibility} />
+        ) : (
+          <HideIcon className="toolbar-icon" size={14} fill={eye} onClick={onChangeVisibility} />
+        )}
+      </Tooltip>
 
-      <ArrowDown
-        className={classnames('toolbar-icon', 'arrow-icon', 'rotate', isCollapse && 'open')}
-        size={14}
-        fill={down}
-        onClick={onChangeCollapse}
-      />
+      <Tooltip
+        overlayClassName="c-rc-tooltip -default"
+        placement="top"
+        trigger={['hover']}
+        overlay={<span>Layer Info</span>}
+        destroyTooltipOnHide
+      >
+        <InfoIcon className="toolbar-icon" size={14} fill={info} onClick={onChangeInfo} />
+      </Tooltip>
+      <Tooltip
+        overlayClassName="c-rc-tooltip -default"
+        placement="top"
+        trigger={['hover']}
+        overlay={<span>{isCollapse ? 'Collapse layer' : 'Expand layer'}</span>}
+        destroyTooltipOnHide
+      >
+        <ArrowDown
+          className={classnames('toolbar-icon', 'arrow-icon', 'rotate', isCollapse && 'open')}
+          size={14}
+          fill={down}
+          onClick={onChangeCollapse}
+        />
+      </Tooltip>
     </div>
   );
 }
