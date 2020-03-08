0985e04ae56c52b7ca1ab3ca5ddd99bbb182a1f3
ruijadom <ruijadomingues@gmail.com>
2020-03-08 22:24:49 +0000

    timeline

--- src/components/Legend/LegendItemTypes/TypeTimeline/index.js
+++ src/components/Legend/LegendItemTypes/TypeTimeline/index.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import Slider from 'rc-slider';
 import PropTypes from 'prop-types';
-import { convertToYear } from '../../../Common/Dates/index';
+import { convertToYear } from '../../../Common/Dates';
 const Range = Slider.Range;
 import './style.scss';
 
-function log(value) {
-  console.log(value); //eslint-disable-line
-}
 class TypeTimeline extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      timeline: this.props.timeline
+      timeline: this.props.timeline,
+      start: 0,
+      end: 100
     };
+    this.log = this.log.bind(this);
   }
 
-  componentDidMount() {}
+  log(value) {
+    this.setState({
+      start: value[0],
+      end: value[1]
+    });
+    console.log(this.state);
+  }
 
   render() {
     const { timeline } = this.state;
     const startDate = convertToYear(timeline.minDate);
     const endDate = convertToYear(timeline.maxDate);
 
+    const { start, end } = this.state;
+
     return (
       <div className="timeline">
         <div className="timeline-item-name">
-          <Range allowCross={false} defaultValue={[0, 20]} onChange={log} reverse />
+          <Range allowCross={false} defaultValue={[start, end]} onChange={this.log} reverse />
           <div className="timeline-item-date">
             <div className="timeline-minDate">{startDate}</div>
             <div className="timeline-maxDate">{endDate}</div>
